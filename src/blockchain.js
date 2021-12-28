ff372888ed670d828057396455ba1e475f97b950
JeremiahAfriprud <87825568+JeremiahAfriprud@users.noreply.github.com>
2021-12-28 08:13:19 +0100

    added BlockChain class

---
+++ src/blockchain.js
@@ -0,0 +1,34 @@
+/**
+ *                          Blockchain Class
+ *  The Blockchain class contain the basics functions to create private blockchain
+ *  It uses libraries like `crypto-js` to create the hashes for each block and `bitcoinjs-message` 
+ *  to verify a message signature. The chain is stored in the array
+ *  `this.chain = [];`. Of course each time you run the application the chain will be empty because an array
+ *  isn't a persisten storage method.
+ *  
+ */
+
+const SHA256 = require('crypto-js/sha256');
+const BlockClass = require('./block.js');
+const bitcoinMessage = require('bitcoinjs-message');
+
+class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.height = -1;
+        this.initializeChain();
+    }
+
+    /**
+     * This method will check for the height of the chain and if there isn't a Genesis Block it will create it.
+     */
+    async initializeChain() {
+        if( this.height === -1){
+            let block = new BlockClass.Block({data: 'Genesis Block'});
+            await this._addBlock(block);
+        }
+    }
+
+}
+
+module.exports.Blockchain = Blockchain;   
